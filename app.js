51e11d993d4da0c645adb168a2e95b80bbb12463
Ari <arianaecotrone@gmail.com>
2026-08-26 16:36:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const WHATSAPP_NUMBER = "5491134442814";
 // ID de la Google Sheet que funciona como "base de datos" del catálogo.
 // Se saca de la URL de la hoja: https://docs.google.com/spreadsheets/d/ESTE_ES_EL_ID/edit
 // Dejalo vacío ("") para que la página use solo los datos embebidos en products.fallback.js.
-const SHEET_ID = "1UC3R2aKW-p5EsGKgd-M5a-hQEJsrQsOb6Mx9vKK17pI";
+const SHEET_ID = "1i19uO_mq9iejI-e5fdFf3joGCJCphSmACIp2vqzV9Sg"; 
 
 // Nombre de la pestaña (tab) dentro de esa Google Sheet que tiene la tabla de productos.
 const SHEET_NAME = "productos";
